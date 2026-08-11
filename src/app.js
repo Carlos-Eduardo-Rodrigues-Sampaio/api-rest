e1d950a27ab5b191ab6c80adcb328b2d6d703eaa
Carlos Eduardo Rodrigues Sampaio <ce_sampaio@hotmail.com>
2026-08-11 18:20:53 -0300

    third commit

--- src/app.js
+++ src/app.js
@@ -1,14 +1,9 @@
-const express = require('express');
-const app = express();
-
-const port = 3000;
+import express from 'express'
+const app = express()
 
 // Criando a rota raiz
 app.get('/', (req, res) => {
-    res.send('Minha API REST com express!');
+    res.send('Minha API REST com express!')
 })
 
-// Iniciando o servidor
-app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
-})
+export default app
