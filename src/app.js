a9e092c3aa11c551f7401015cc276fa403337aa3
Carlos Eduardo Rodrigues Sampaio <ce_sampaio@hotmail.com>
2026-08-11 19:27:42 -0300

    Criando um Mock

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 import express from 'express'
 const app = express()
 
+// Mock
+const lista = [
+    {id: 1, nome: 'Bruno', curso: 'ADS'},
+    {id: 2, nome: 'Maria', curso: 'ADS'},
+    {id: 3, nome: 'Çara', curso: 'ADS'},
+    {id: 4, nome: 'José', curso: 'ADS'},
+]
+
 // Criando a rota raiz
 app.get('/', (req, res) => {
     res.send('Minha API REST com express!')
@@ -8,7 +16,7 @@ app.get('/', (req, res) => {
 
 // Rota lista
 app.get('/lista', (req, res) => {
-    res.send('Lista do SENAC');
+    res.send(lista);
 });
 
 export default app
