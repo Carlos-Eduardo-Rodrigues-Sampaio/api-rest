cfdaf2b5feec6b07b7ab6d386301eba147bd9314
Carlos Eduardo Rodrigues Sampaio <ce_sampaio@hotmail.com>
2026-08-11 21:03:27 -0300

    Implementando o DELETE Pelo Id

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express'
 const app = express()
 
+// Express deve interpretar o corpo (body) como JSON.
+app.use(express.json());
+
 // Mock
 const alunos = [
     {id: 1, nome: 'Bruno', curso: 'ADS'},
@@ -9,6 +12,15 @@ const alunos = [
     {id: 4, nome: 'José', curso: 'ADS'},
 ]
 
+// Função auxiliar
+function buscarAlunoPorId(id) {
+    return alunos.filter( aluno => aluno.id == id )
+}
+
+function buscarIndexAluno(id) {
+    return alunos.findIndex( aluno => aluno.id == id )
+}
+
 // Criando a rota raiz
 app.get('/', (req, res) => {
     res.send('Minha API REST com express!')
@@ -16,7 +28,7 @@ app.get('/', (req, res) => {
 
 // Rota lista alunos GET
 app.get('/alunos', (req, res) => {
-res.status(200).send(alunos);
+    res.status(200).send(alunos);
 });
 
 // Rota lista alunos POST
@@ -24,5 +36,15 @@ app.post('/alunos', (req, res) => {
     alunos.push(req.body)
     res.status(201).send('Alunos cadastrado com sucesso!');
 })
+
+// Delete
+app.delete('/alunos/:id', (req, res) => {
+    let index = buscarIndexAluno(req.params.id)
+    // console.log(index)
+    alunos.splice(index, 1)
+    res.send(`Aluno com id ${req.params.id} excluido com sucesso!`)
+ })
+
+ // 
 
 export default app
